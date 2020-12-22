3ecdfa8b662f8e99ee1c7c28673d7fac2c19001a
M D <md@ms-macbook-pro.home>
2020-12-22 18:55:47 +0000

    RealNews class

--- real_news/real_news.py
+++ real_news/real_news.py
@@ -20,88 +20,93 @@ from utils import parse_record, get_timestamp
 # s3client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 # s3client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 
-
-file_key = '../aws_key.csv'
-# Looks for credentials in local file
-if os.path.exists(file_key):
-    keys = pd.read_csv(file_key)
-    AWS_ACCESS_KEY = keys['Access key ID'][0]
-    AWS_SECRET_KEY = keys['Secret access key'][0]
-# Looks for credentials as environment variables (recommended)
-else:
-    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
-    AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
-
-s3client = boto3.client('s3', 
-                    aws_access_key_id=AWS_ACCESS_KEY,
-                    aws_secret_access_key=AWS_SECRET_KEY)
 # python real_news.py --path crawl-data/CC-NEWS/2020/10/CC-NEWS-20201015140400-00177.warc.gz
+# python deep_fake_index/real_news/real_news.py --job_type=TEST --allow_all True --path crawl-data/CC-NEWS/2020/12/CC-NEWS-20201215235550-01185.warc.gz
+class RealNews():
+    def __init__(self, args):
+        self.args = args
+        # Looks for credentials in local file
+        file_key = args.file_key
+        if os.path.exists(file_key):
+            keys = pd.read_csv(file_key)
+            AWS_ACCESS_KEY = keys['Access key ID'][0]
+            AWS_SECRET_KEY = keys['Secret access key'][0]
+        # Looks for credentials as environment variables (recommended)
+        else:
+            AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+            AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
 
-def make_response(outcome, dt0, dt1, job_type, payload):
-    host_name = socket.gethostname() 
-    host_ip = socket.gethostbyname(host_name) 
-    dt0 = get_timestamp(dt0)
-    dt1 = get_timestamp(dt1)
-    out = {
-        'started': dt0,
-        'finished': dt1,
-        'outcome': outcome,
-        'job_type': job_type,
-        'payload': payload,
-        'host_name': host_name,
-        'host_ip': host_ip
-    }
-    file_path = f'logs/job_{job_type}_{dt1}.txt'
-    with open(file_path, 'w') as f:
-        json.dump(out,f)
-    return file_path
+        s3client = boto3.client('s3', 
+                        aws_access_key_id=AWS_ACCESS_KEY,
+                        aws_secret_access_key=AWS_SECRET_KEY)
+        self.s3client = s3client
 
-def parse_news_file(args, output_path):
-    archive_date = args.path.split('/')[1]
-    rest = '_'.join(args.path.split('/')[2:])
-    allow_all = args.allow_all
-    if allow_all == False:
-        allow_all = None
-    print('Allow all',allow_all==True)
+    def run(self): 
 
-    out_prefix = 'propaganda-' if args.propaganda else ''
-    out_key = '{}{}/{}.jsonl'.format(out_prefix, archive_date, rest)
+        dt0 = datetime.datetime.now() 
 
-    with TemporaryFile(mode='w+b', dir=output_path) as warctemp:
-        s3client.download_fileobj('commoncrawl', args.path, warctemp)
-        warctemp.seek(0)
+        try:    
+            if self.args.job_type == 'TEST':
+                time.sleep(2)
+                payload = 'TEST'
+            elif self.args.job_type == 'CC_NEWS':
+                output_path = './'
+                print('started',dt0)
+                payload = self.parse_news_file(output_path)
 
-        with NamedTemporaryFile(mode='w', dir=output_path) as f:
-            for record in tqdm(ArchiveIterator(warctemp, no_record_parse=False)):
-                for parsed_record in parse_record(record, propaganda=args.propaganda, allow_all=allow_all):
-                    f.write(json.dumps(parsed_record) + '\n')
+            dt1 = datetime.datetime.now()
+            print('done',dt1)
+            outcome = 'Success'
+        except:
+            outcome = 'Failed'
+            dt1 = datetime.datetime.now()
+        
+        file_name = self.make_response(outcome, dt0, dt1, job_type = args.job_type, payload = payload)
+        self.s3client.upload_file(file_name, args.bucket_name, file_name)
 
-            s3client.upload_file(f.name, args.bucket_name, out_key)
+    def parse_news_file(self, output_path):
+        archive_date = self.args.path.split('/')[1]
+        rest = '_'.join(self.args.path.split('/')[2:])
+        allow_all = self.args.allow_all
+        if allow_all == False:
+            allow_all = None
+        print('Allow all',allow_all==True)
 
-        print("I guess I'm done now")
-    return rest
+        out_prefix = 'propaganda-' if self.args.propaganda else ''
+        out_key = '{}{}/{}.jsonl'.format(out_prefix, archive_date, rest)
 
-def main(args): 
-    dt0 = datetime.datetime.now() 
+        with TemporaryFile(mode='w+b', dir=output_path) as warctemp:
+            self.s3client.download_fileobj('commoncrawl', args.path, warctemp)
+            warctemp.seek(0)
 
-    try:    
-        if args.job_type == 'TEST':
-            time.sleep(2)
-            payload = 'TEST'
-        elif args.job_type == 'CC_NEWS':
-            output_path = './'
-            print('started',dt0)
-            payload = parse_news_file(args, output_path)
+            with NamedTemporaryFile(mode='w', dir=output_path) as f:
+                for record in tqdm(ArchiveIterator(warctemp, no_record_parse=False)):
+                    for parsed_record in parse_record(record, propaganda=args.propaganda, allow_all=allow_all):
+                        f.write(json.dumps(parsed_record) + '\n')
 
-        dt1 = datetime.datetime.now()
-        print('done',dt1)
-        outcome = 'Success'
-    except:
-        outcome = 'Failed'
-        dt1 = datetime.datetime.now()
-    
-    file_name = make_response(outcome, dt0, dt1, job_type = args.job_type, payload = payload)
-    s3client.upload_file(file_name, args.bucket_name, file_name)
+                self.s3client.upload_file(f.name, args.bucket_name, out_key)
+
+            print("I guess I'm done now")
+        return rest
+
+    def make_response(self,outcome, dt0, dt1, job_type, payload):
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        dt0 = get_timestamp(dt0)
+        dt1 = get_timestamp(dt1)
+        out = {
+            'started': dt0,
+            'finished': dt1,
+            'outcome': outcome,
+            'job_type': job_type,
+            'payload': payload,
+            'host_name': host_name,
+            'host_ip': host_ip
+        }
+        file_path = f'logs/job_{job_type}_{dt1}.txt'
+        with open(file_path, 'w') as f:
+            json.dump(out,f)
+        return file_path        
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Collects and parses a news file')
@@ -119,6 +124,10 @@ if __name__ == '__main__':
                         type=str,
                         default = 'CC-NEWS',
                         help='Takes CC-NEWS or TEST, defaults to TEST')
+    parser.add_argument('--file_key', 
+                        type=str,
+                        default = 'aws_key.csv',
+                        help='Path to file with AWS creds')
     parser.add_argument('--allow_all', 
                         type=bool,
                         default = True,
@@ -129,4 +138,5 @@ if __name__ == '__main__':
     # 
     # aws s3 ls s3://commoncrawl/crawl-data/CC-MAIN-2020-05/segments/1579250589560.16/ --no-sign-request
     # aws s3 ls s3://commoncrawl/crawl-data/CC-NEWS/2020/10/CC-NEWS-20201015* --no-sign-request
-    main(args)
+    rn = RealNews(args)
+    rn.run()
